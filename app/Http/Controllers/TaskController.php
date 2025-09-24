a37f3f368d5439f7318af94e23b1cbaf3fba775b
vanessa50w <vanessacarine1810@gmail.com>
2025-09-24 15:46:18 -0300

    Criado TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tasks = auth()->user()->tasks;
+        return view('tasks.index', compact('tasks'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+         return view('tasks.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        auth()->user()->tasks()->create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'status' => 'pendente',
+        ]);
+
+        return redirect()->route('tasks.index')->with('success', 'Tarefa criada com sucesso!');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+         $this->authorize('view', $task);
+        return view('tasks.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $this->authorize('update', $task);
+        return view('tasks.edit', compact('task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $this->authorize('update', $task);
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|string|in:pendente,concluída',
+        ]);
+
+        $task->update($request->all());
+
+        return redirect()->route('tasks.index')->with('success', 'Tarefa atualizada com sucesso!');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $this->authorize('delete', $task);
+
+        $task->delete();
+
+        return redirect()->route('tasks.index')->with('success', 'Tarefa excluída com sucesso!');
+    }
+
+} 
